8bd364b6b55fed9b357aff035745025a913a14f9
ououopel0001 <71679877+ououopel0001@users.noreply.github.com>
2021-01-16 18:25:21 +0800

    對話框

---
+++ 3D_End_107051041/Assets/scripts/NPC.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    [Header("NPC資料")]
+    public NPCdata data;
+}
---
+++ 3D_End_107051041/Assets/scripts/NPCdata.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "NPC資料", menuName = "OUBAO/NPC資料")]
+public class NPCdata : ScriptableObject
+{
+    [Header("第一段對話"), TextArea(1,5)]
+    public string dialougA;
+    [Header("第二段對話"), TextArea(1, 5)]
+    public string dialougB;
+    [Header("第三段對話"), TextArea(1, 5)]
+    public string dialougC;
+    [Header("未完成任務數量")]
+    public int count;
+    [Header("以完成任務數量")]
+    public int counted;
+}
